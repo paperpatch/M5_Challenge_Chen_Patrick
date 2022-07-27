43c60eadea7bea3239f7f2763ab196152c097db0
paperpatch <patchen21@gmail.com>
2022-07-27 19:56:03 -0400

    converted most of starter to gamestore invoicing

--- gamestore-invoicing/sql/data.sql
+++ gamestore-invoicing/sql/data.sql
@@ -1,13 +1,4 @@
-use game_store;
-
-insert into game(title, esrb_rating, description, price, studio, quantity)
-	values ('World of Goo', 'PG', 'puzzle video game', 12.99, '2D Boy', 25);
-
-insert into console(model, manufacturer, memory_amount, processor, price, quantity)
-	values ('Nintendo Switch', 'Nintendo', '4GB', 'ARM 4 Cortex-A57', 299.99, 7);
-    
-insert into tshirt (size, color, description, price, quantity)
-	values ('Med', 'Yellow', 'Science', 14.95, 0);
+use game_store_invoicing;
     
 insert into invoice
 	(`name`, street, city, state, zipcode, item_type, item_id, unit_price, quantity, subtotal, tax, processing_fee, total)
